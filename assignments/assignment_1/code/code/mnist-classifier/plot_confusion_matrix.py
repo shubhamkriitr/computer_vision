7c57931a8768ac106e17ae75edeba0fc1bc11d57
Shubham Kumar <kumarsh@student.ethz.ch>
2021-10-22 17:54:02 +0200

    plot code fix

--- assignments/assignment_1/code/code/mnist-classifier/plot_confusion_matrix.py
+++ assignments/assignment_1/code/code/mnist-classifier/plot_confusion_matrix.py
@@ -71,5 +71,7 @@ if __name__ == '__main__':
     plt.ylabel('annotation')
     for i in range(10):
         for j in range(10):
-            plt.text(i, j, '%d' % (confusion_matrix[i, j]), ha='center', va='center', color='w', fontsize=12.5)
+            # plt.text(i, j, '%d' % (confusion_matrix[i, j]), ha='center', va='center', color='w', fontsize=12.5)
+            # FIX added for axis swap
+            plt.text(i, j, '%d' % (confusion_matrix[j, i]), ha='center', va='center', color='w', fontsize=12.5)
     plt.show()
