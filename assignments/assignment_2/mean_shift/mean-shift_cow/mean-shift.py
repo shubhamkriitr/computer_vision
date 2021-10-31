e3eb6d0b70d038e1a04fd214f4a95bc9ca9bb5c1
Shubham Kumar <kumarsh@student.ethz.ch>
2021-10-31 19:33:25 +0100

    parallel meanshift batch function - not others

--- assignments/assignment_2/mean_shift/mean-shift_cow/mean-shift.py
+++ assignments/assignment_2/mean_shift/mean-shift_cow/mean-shift.py
@@ -13,11 +13,15 @@ from skimage.transform import rescale
 import multiprocessing
 from concurrent.futures import ProcessPoolExecutor
 
-NUM_WORKERS = 4
-try:
-    NUM_WORKERS = multiprocessing.cpu_count()
-except Exception as exc:
-    print("Could not get cpu count")
+from torch.autograd import backward
+torch.manual_seed(0)
+np.random.seed(0)
+NUM_WORKERS = 8
+MAIN_EXECUTOR = ProcessPoolExecutor(max_workers=NUM_WORKERS)
+# try:
+#     # NUM_WORKERS = multiprocessing.cpu_count()
+# except Exception as exc:
+#     print("Could not get cpu count")
 
 def get_batch_indices(begin_, end_, workers):
     batches = []
@@ -42,15 +46,16 @@ def distance(x, X):
 
 
 def _distance_batch(args):
+    # print(f"dist: {args[0].shape} - {args[1].shape}")
     return distance(args[0], args[1])
 
 def distance_batch(x, X):
     batch_idx = get_batch_indices(0, X.shape[0], NUM_WORKERS)
     args_arr = [(x, X[start_:end_]) for start_, end_ in batch_idx]
     results = []
-    with ProcessPoolExecutor(max_workers=NUM_WORKERS) as executor:
-        for sub_result in executor.map(_distance_batch, args_arr):
-            results.append(sub_result)
+    executor = MAIN_EXECUTOR
+    for sub_result in executor.map(_distance_batch, args_arr):
+        results.append(sub_result)
     return torch.cat(results, dim=0)
 
 def gaussian(dist, bandwidth):
@@ -61,6 +66,7 @@ def update_point(weight, X):
 
 
 def _update_point_nodiv(args):
+    # print(f"update: {args[0].shape} - {args[1].shape}")
     return (args[0]*args[1].T).T.sum(dim=0)
 
 def update_point_batch(weight, X):
@@ -69,9 +75,10 @@ def update_point_batch(weight, X):
     batch_idx = get_batch_indices(0, X.shape[0], NUM_WORKERS)
     args_arr = [(weight[start_:end_], X[start_:end_]) for start_, end_ in batch_idx]
     results = []
-    with ProcessPoolExecutor(max_workers=NUM_WORKERS) as executor:
-        for sub_result in executor.map(_update_point_nodiv, args_arr):
-            running_sum += sub_result
+    executor = MAIN_EXECUTOR
+    # with ProcessPoolExecutor(max_workers=NUM_WORKERS) as executor:
+    for sub_result in executor.map(_update_point_nodiv, args_arr):
+        running_sum += sub_result
     return running_sum/wt_sum
 
 def meanshift_step(X, bandwidth=2.5):
@@ -82,20 +89,36 @@ def meanshift_step(X, bandwidth=2.5):
         X_[i] = update_point(weight, X)
     return X_
 
-def meanshift_step_batch(X, bandwidth=2.5):
-    X_ = X.clone()
-    for i, x in enumerate(X):
-        dist = distance_batch(x, X)
+def _meanshift_step_batch(args):
+    X_part = args[0]
+    X = args[1]
+    bandwidth = args[2]
+    X_ = X_part.clone()
+    # for i, x in enumerate(X):
+    for i in range(X_part.shape[0]):
+        x = X_part[i]
+        dist = distance(x, X)
         weight = gaussian(dist, bandwidth)
-        X_[i] = update_point_batch(weight, X)
+        X_[i] = update_point(weight, X)
     return X_
+
+def meanshift_step_batch(X, bandwidth=2.5):
+    batch_idx = get_batch_indices(0, X.shape[0], NUM_WORKERS)
+    args_arr = [(X[start_:end_], X, bandwidth) for start_, end_ in batch_idx]
+    X_ = []
+    with ProcessPoolExecutor(max_workers=NUM_WORKERS) as executor:
+        for x_ in executor.map(_meanshift_step_batch, args_arr):
+            X_.append(x_)
+    
+    return torch.cat(X_)
+
 
 def meanshift(X):
     X = X.clone()
     for _ in range(20):
-        X = meanshift_step(X)   # slow implementation
-        # print(f"Using {NUM_WORKERS} workers")
-        # X = meanshift_step_batch(X)   # fast implementation
+        # X = meanshift_step(X)   # slow implementation
+        print(f"Using {NUM_WORKERS} workers")
+        X = meanshift_step_batch(X)   # fast implementation
         # 
     return X
 
@@ -125,3 +148,4 @@ result_image = colors[labels].reshape(shape)
 result_image = rescale(result_image, 1 / scale, order=0, multichannel=True)     # resize result image to original resolution
 result_image = (result_image * 255).astype(np.uint8)
 io.imsave('result.png', result_image)
+MAIN_EXECUTOR.shutdown()
