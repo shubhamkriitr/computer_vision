0e6bf25d7ea94813180a17ef47ea32b4a417477f
Shubham Kumar <kumarsh@student.ethz.ch>
2021-11-18 15:25:16 +0100

    added callback to display error

--- assignments/assignment_3/code/impl/calib/opt.py
+++ assignments/assignment_3/code/impl/calib/opt.py
@@ -48,6 +48,15 @@ def OptimizeProjectionMatrix(P, points2D, points3D):
     # Make sure this is fixed by keeping the last component close to 1.
     scale_constraint = {'type': 'eq', 'fun': lambda x : x[11] - 1}
 
+    iter_count = 0
+    def callback_to_display_err(*args, **kwargs):
+      nonlocal iter_count
+      iter_count += 1
+      x = args[0]
+      err = np.linalg.norm(ImageResiduals(np.reshape(x, (3, 4)), points2D, points3D)) ** 2
+      print(f"Iteration: {iter_count} Error: {err}")
+
     # Make sure the scale constraint is fulfilled at the beginning
-    result = spo.minimize(f, np.reshape(P / P[2,3], 12), options={'disp': True}, constraints=[scale_constraint], tol=1e-12)
+    result = spo.minimize(f, np.reshape(P / P[2,3], 12), options={'disp': True}, constraints=[scale_constraint], tol=1e-12,
+                           callback=callback_to_display_err)
     return np.reshape(result.x, (3, 4))
