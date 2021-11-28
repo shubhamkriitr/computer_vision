d4e1f7919f8c15345bca3cad38c64ef066f342d2
Shubham Kumar <kumarsh@student.ethz.ch>
2021-11-28 20:31:16 +0100

    fix padding for convt

--- assignments/assignment_4/mvs/models/module.py
+++ assignments/assignment_4/mvs/models/module.py
@@ -57,9 +57,11 @@ class SimlarityRegNet(nn.Module):
         self.conv_2 = nn.Conv2d(in_channels=16, out_channels=32,
                 kernel_size=(3, 3), stride=2, padding=(1, 1))
         self.convt_0 = nn.ConvTranspose2d(in_channels=32, out_channels=16,
-                kernel_size=(3,3), stride=2)
+                kernel_size=(3,3), stride=2,
+                padding=1, output_padding=1) 
         self.convt_1 = nn.ConvTranspose2d(in_channels=16, out_channels=8,
-                kernel_size=(3,3), stride=2)
+                kernel_size=(3,3), stride=2,
+                padding=1, output_padding=1)
         self.final_conv2d = nn.Conv2d(in_channels=8, out_channels=1,
                 kernel_size=(3, 3), stride=1, padding=(1, 1))
         self.relu = nn.ReLU()
