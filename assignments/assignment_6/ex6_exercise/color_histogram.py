0a9b0930df7c997ebd3871c82b0042cd363f5858
Shubham Kumar <kumarsh@student.ethz.ch>
2021-12-21 02:54:38 +0100

    fix indexing issue in color_histogram

--- assignments/assignment_6/ex6_exercise/color_histogram.py
+++ assignments/assignment_6/ex6_exercise/color_histogram.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 PIXEL_MIN_VAL = 0
 PIXEL_MAX_VAL = 255
@@ -6,12 +7,12 @@ PIXEL_MAX_VAL = 255
 PIXEL_VALUE_RANGE = (PIXEL_MIN_VAL, PIXEL_MAX_VAL) # used in histogram creation
 
 def color_histogram(xmin, ymin, xmax, ymax, frame, hist_bin):
-    frame_patch = frame[xmin:xmax, ymin:ymax, :]
+    frame_patch = frame[ymin:ymax, xmin:xmax, :] # NOTE: y is first dim
 
     hists = []
 
     for i in range(frame_patch.shape[2]):
-        channel_hist = np.histogram(frame_patch[:, :, i],
+        channel_hist, bins_ = np.histogram(frame_patch[:, :, i],
                                     range=PIXEL_VALUE_RANGE,
                                     bins=hist_bin)
         hists.append(channel_hist)
