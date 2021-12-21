320003a3eb5e31450a07cdcb89282d88b3e2e710
Shubham Kumar <kumarsh@student.ethz.ch>
2021-12-21 18:55:10 +0100

    added estimate

--- assignments/assignment_6/ex6_exercise/estimate.py
+++ assignments/assignment_6/ex6_exercise/estimate.py
@@ -1,2 +1,6 @@
-def estimate():
-    pass
+import numpy as np
+
+def estimate(particles, particles_weight):
+    weighted_particles = particles_weight*particles
+    expected_particle = np.sum(weighted_particles, axis=0)/np.sum(particles_weight)
+    return expected_particle
